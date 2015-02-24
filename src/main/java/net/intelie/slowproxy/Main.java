27ea5cfd4f33beebfb74aa2940ff4737214fb34e
Juan Lopes <me@juanlopes.net>
2015-02-24 14:17:03 -0300

    usage

--- src/main/java/net/intelie/slowproxy/Main.java
+++ src/main/java/net/intelie/slowproxy/Main.java
@@ -1,6 +1,8 @@
 package net.intelie.slowproxy;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,14 +12,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
     @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) throws Exception {
+        if (args.length == 0)
+            printUsageAndExit();
         final Options options;
         try {
             options = Options.parse(args);
         } catch (Exception e) {
             System.out.println(e.getClass().getName() + ": " + e.getMessage());
-            System.out.println("usage: slowproxy [@<speed>] <local port> [<remote host>:<remote port>]...");
-            System.out.println("example: slowproxy @56kbps 1234 somehost:80");
-            System.exit(1);
+            printUsageAndExit();
             return;
         }
 
@@ -39,6 +41,16 @@ public class Main {
             }
         }
         System.exit(0);
+    }
+
+    private static void printUsageAndExit() throws IOException {
+        InputStream input = Main.class.getResourceAsStream("/usage.txt");
+        byte[] buffer = new byte[1024]; // Adjust if you want
+        int bytesRead;
+        while ((bytesRead = input.read(buffer)) != -1) {
+            System.out.write(buffer, 0, bytesRead);
+        }
+        System.exit(1);
     }
 
     private static int setNextHost(String line, int hostCount, int currentHost) {
